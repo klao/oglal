8c93c5cdfbeaefe553b8a842ed84f30d6546d1ea
Mihaly Barasz <klao@nilcons.com>
2015-12-20 21:09:32 +0100

    OpenAL and OpenGL together

--- src/Main.hs
+++ src/Main.hs
@@ -1,50 +1,114 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
-import Control.Monad
-import Graphics.Rendering.OpenGL as GL
-import Graphics.UI.GLFW as GLFW
+import           Control.Concurrent
+import           Control.Concurrent.STM
+import           Control.Lens
+import           Control.Monad
+import qualified Data.Vector.Unboxed as VU
+import           Graphics.Rendering.OpenGL as GL
+import           Graphics.UI.GLFW as GLFW
 
-drawingStuff :: GLFW.Window -> IO ()
-drawingStuff win = do
+import           My.Sound
+
+data Event
+  = EvtWinClose
+  | EvtResize !Int !Int
+  deriving (Eq, Show)
+
+data State
+  = State { _stGoOn :: !Bool
+          , _stW :: !Int
+          , _stH :: !Int
+          }
+  deriving (Eq, Show)
+makeLenses ''State
+
+stWH :: Functor f => ((Int, Int) -> f (Int, Int)) -> State -> f State
+stWH f s@(State {..}) = fmap (\(w,h) -> s { _stW = w, _stH = h }) $ f (_stW, _stH)
+
+iniState :: State
+iniState = State True 0 0
+
+processEvents :: TQueue Event -> State -> IO State
+processEvents evch = go
+  where
+    go st = do
+      me <- atomically $ tryReadTQueue evch
+      case me of
+        Nothing -> return st
+        Just ev -> do
+          case ev of
+            EvtWinClose -> go $ st & stGoOn .~ False
+            EvtResize w h -> do
+              viewport $= (Position (-fromIntegral w) 0,
+                           Size (2 * fromIntegral w) (fromIntegral h))
+              GL.loadIdentity
+              scale (1 /  fromIntegral w) 0.5 (1.0 :: GLfloat)
+
+              go $ st & stWH .~ (w,h)
+
+drawingStuff :: GLFW.Window -> TQueue Event -> TQueue (GLfloat, GLfloat) -> IO ()
+drawingStuff win evch spch = do
+  GLFW.setWindowCloseCallback win $ Just $ \_ ->
+    atomically $ writeTQueue evch EvtWinClose
+  GLFW.setWindowSizeCallback win $ Just $ \_ x y ->
+    atomically $ writeTQueue evch $ EvtResize x y
+
   GLFW.swapInterval 1
   depthFunc $= Just Always
   clearColor $= Color4 1 1 1 1
-  forever $ do
+  loop (iniState & stW .~ 800 & stH .~ 600) []
+
+  where
+  loop st l = do
+    GLFW.pollEvents
+    st' <- processEvents evch st
+    l' <- take 10000 <$> consumeTQueue spch l
+
     clear [ColorBuffer]
     color $ Color4 0 0 0 (1 :: GLfloat)
 
-    -- This works:
-    multisample $= Enabled
-    -- These don't work
-    -- lineSmooth $= Enabled
-    -- polygonSmooth $= Enabled
-    -- hint LineSmooth $= Nicest
-    -- hint PolygonSmooth $= Nicest
-    renderPrimitive Polygon $ do
-      vertex $ Vertex2 0 (0 :: GLfloat)
-      vertex $ Vertex2 1 (0.1 :: GLfloat)
-      vertex $ Vertex2 0 (0.67826 :: GLfloat)
+    let w = fromIntegral $ st' ^. stW
+    renderPrimitive Lines $ do
+      forM_ (zip [w,w-1..(1::GLfloat)] l') $ \(x,(y0,y1)) -> do
+        vertex $ Vertex2 x y0
+        vertex $ Vertex2 x y1
 
-    multisample $= Disabled
-    -- lineSmooth $= Disabled
-    -- polygonSmooth $= Disabled
-    -- hint LineSmooth $= DontCare
-    -- hint PolygonSmooth $= DontCare
-    renderPrimitive Polygon $ do
-      vertex $ Vertex2 0 (0 :: GLfloat)
-      vertex $ Vertex2 (-1) (0.1 :: GLfloat)
-      vertex $ Vertex2 0 (0.67826 :: GLfloat)
-    -- swapBuffers
     GLFW.swapBuffers win
 
+    when (st ^. stGoOn) $ loop st' l'
+
+consumeTQueue :: TQueue a -> [a] -> IO [a]
+consumeTQueue q = go
+  where
+    go l = do
+      mx <- atomically $ tryReadTQueue q
+      case mx of
+        Just x -> go (x:l)
+        Nothing -> return l
+
+soundProcessor :: TQueue (GLfloat, GLfloat) -> IO ()
+soundProcessor spch = do
+  bch <- newTQueueIO
+  _ <- forkIO $ startCapture 128 (atomically . writeTQueue bch)
+  forever $ do
+    b <- atomically $ readTQueue bch
+    atomically $ writeTQueue spch $ (VU.minimum b, VU.maximum b) & both %~ realToFrac
 
 main :: IO ()
 main = do
   GLFW.setErrorCallback $ Just $ \e s -> putStrLn $ show e ++ ": " ++ s
   True <- GLFW.init
-  GLFW.windowHint $ GLFW.WindowHint'Samples 8
   Just win <- GLFW.createWindow 800 600 "GLFW proba" Nothing Nothing
   GLFW.makeContextCurrent (Just win)
-  drawingStuff win
+  evch <- newTQueueIO
+
+  spch <- newTQueueIO
+  _ <- forkIO $ soundProcessor spch
+
+  drawingStuff win evch spch
   GLFW.destroyWindow win
   GLFW.terminate
