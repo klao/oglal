deb598291f95572ac428a762e8ec1584555a961e
Mihaly Barasz <klao@nilcons.com>
2016-01-03 20:40:39 +0100

    Do fake waveform if device open fails

--- src/My/Sound.hs
+++ src/My/Sound.hs
@@ -32,18 +32,31 @@ captureLoop samples callb dev buf = forever $ do
             l <- peekArray samples buf
             callb (VU.fromListN samples $ map samplesToFloat l)
 
+fakeLoop :: Int -> (Vector Float -> IO ()) -> IO ()
+fakeLoop samples callb = go wave
+  where
+    wave = map (sin . (/100)) [0..]
+    go ss = do
+      let (c,ss') = splitAt samples ss
+      callb (VU.fromListN samples c)
+      threadDelay $ fromIntegral samples * 1000000 `div` frequency
+      go ss'
+
 startCapture
   :: Int                        -- ^ Number of samples to get at a time
   -> (Vector Float -> IO ())    -- ^ Callback to consume recorded samples
   -> IO ()
 startCapture samples callb = do
   -- TODO(klao): is 2*samples enough? Detect overruns?
-  Just dev <- captureOpenDevice Nothing 44100 Mono16 (fromIntegral $ 2 * samples)
-  captureStart dev
-  buf <- callocArray 2048
+  mdev <- captureOpenDevice Nothing 44100 Mono16 (fromIntegral $ 2 * samples)
+  case mdev of
+    Nothing -> fakeLoop samples callb
+    Just dev -> do
+      captureStart dev
+      buf <- callocArray 2048
 
-  captureLoop samples callb dev buf
+      captureLoop samples callb dev buf
 
-  captureStop dev
-  _ <- captureCloseDevice dev
-  return ()
+      captureStop dev
+      _ <- captureCloseDevice dev
+      return ()
